ef00f12b422618099a3e8b5434060cf1fc89cb00
Marvin Lee <48476808+vogelfrey@users.noreply.github.com>
2022-04-08 21:42:03 +0200

    Initial Commit

---
+++ Snaked/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snaked
+//
+//  Created by Marvin Lee Kobert on 08.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Snaked/SnakedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakedApp.swift
+//  Snaked
+//
+//  Created by Marvin Lee Kobert on 08.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SnakedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
